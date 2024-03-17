a2f55059f17a299d1b9bd9def185d0489bfd0d91
pkonkol <31186361+pkonkol@users.noreply.github.com>
2024-03-17 10:37:37 +0100

    gitconfig

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
-sudo apt install -y build-essential whois neovim syncthing tmux ranger fish tldr nmap masscan python-is-python3 exa bat ripgrep fd-find zoxide grc sd
-git clone https://github.com/pkonkol/dotfiles.git
+sudo apt install -y build-essential whois neovim syncthing tmux ranger fish tldr nmap masscan python-is-python3 bat ripgrep fd-find zoxide grc sd translate-shell
+#git clone https://github.com/pkonkol/dotfiles.git
 
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow ssh
-sudo ufw allow 2137
-sudo ufw enable
-systemctl enable user@syncthing
-systemctl start user@syncthing
+#sudo ufw default deny incoming
+#sudo ufw default allow outgoing
+#sudo ufw allow ssh
+#sudo ufw allow syncthing
+#sudo ufw enable
+#systemctl enable user@syncthing
+#systemctl start user@syncthing
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
@@ -16,9 +16,8 @@ curl -sS https://starship.rs/install.sh | sh
 
 chsh -s $(which fish)
 tldr -u
-starship preset gruvbox-rainbow -o ~/.config/starship.toml
-omf install grc bass foreign-env 
+fish -c 'omf install grc bass foreign-env'
 
-source "$HOME/cargo/.env"
-cargo install skim atuin 
-# cargo install ytop bandwhich du-dust procs
+source "$HOME/.cargo/.env"
+cargo install eza atuin git-delta cargo-cache
+# cargo install ytop bandwhich du-dust procs skim 
